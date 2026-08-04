29134014337206107f78f0b4db19a9bb39d57a48
Muhammad Mustafa Amir <mustafaamir398a@gmail.com>
2026-08-04 16:16:28 +0500

    Create 0-Syntrojex.cs

---
+++ Contributors/0-Syntrojex.cs
@@ -0,0 +1,11 @@
+// Code snippet by Syntrojex
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "World";
+        Console.WriteLine($"Hello, {name}! Welcome to the Contributing repo 🎉");
+    }
+}
